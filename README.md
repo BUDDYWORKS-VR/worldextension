c292037408f8c42ce7ee920f865a0ddadfa20bd8
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2025-04-08 19:54:30 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+<div align="center">
+
 <a href="https://buddyworks.wtf"><img width=50% src="https://splash.buddyworks.wtf/tckAqsHD.png"></img></a>  
-# VPM Package Template
-A template for any new package releases that are supposed to interface with the VPM Listing.
+# BUDDYWORKS World Extension
+*Building blocks for your world logic~*
+
+[![Generic badge](https://img.shields.io/github/downloads/BUDDYWORKS-VR/worldextension/total?label=Downloads)](https://github.com/BUDDYWORKS-VR/worldextension/releases/latest)
+[![Generic badge](https://img.shields.io/badge/License-MIT-informational.svg)](https://github.com/BUDDYWORKS-VR/worldextension/blob/main/LICENSE)
+[![Generic badge](https://img.shields.io/badge/Unity-2022.3.22f1-red.svg)](https://unity3d.com/unity/whats-new/2022.3.22)
+
+[![Generic badge](https://img.shields.io/discord/1115323445316702269?color=%237289da&label=DISCORD&logo=Discord&style=for-the-badge)](https://discord.buddyworks.wtf/)
+[![Generic badge](https://img.shields.io/endpoint.svg?url=https%3A%2F%2Fshieldsio-patreon.vercel.app%2Fapi%3Fusername%3Dbuddy_de%26type%3Dpatrons&style=for-the-badge)](https://www.patreon.com/c/buddy_de)
+  
+</div>
+
+Crediting:
+
+If you found this helpful, mentioning it in your world's credits board would be neat.
+
+BUDDYWORKS World Extension by Buddy_DE
---
+++ Scripts/TriggerBlock.cs
@@ -0,0 +1,151 @@
+﻿using UdonSharp;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.Persistence;
+
+namespace BUDDYWORKS.WorldExtension
+{
+    [AddComponentMenu("BUDDYWORKS/Trigger Block")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class TriggerBlock : UdonSharpBehaviour
+    {
+        [Header("Send a message to a different Udon Behaviour.")]
+        [SerializeField] private UdonSharpBehaviour outputMessageBehaviour;
+        [SerializeField] private string _outputMessage = "OnTrigger";
+
+        [Header("Toggle a GameObject when triggered.")]
+        [SerializeField] private GameObject outputGameObject;
+        [SerializeField] private bool _isSynced;
+
+        [Header("Persistence Settings for GameObject Toggles, avoid using with Synced.")]
+        [SerializeField] private bool _isPersisted;
+        [Tooltip("Set this string to something unique when using Is Persisted. Using the same Identifier might cause other things to trigger, too.")]
+        [SerializeField] private string _persistenceIdentifier = "unique.identifier";
+
+        bool toggled; // Local variable for non-synced and persisted toggling.
+        [UdonSynced] bool toggledSynced; // Synced variable for synced toggling.
+
+        [Header("Run when this GameObject is getting enabled or disabled. (Only for Local tasks.)")]
+        [SerializeField] private bool _onEnableActive;
+        [SerializeField] private bool _onDisableActive;
+
+        void Start()
+        {
+            // Initialize state based on persistence, sync, and GameObject's active state.
+            if (_isPersisted)
+            {
+                LoadPersistentState();
+            }
+            else if (_isSynced)
+            {
+                toggledSynced = outputGameObject.activeSelf;
+            }
+            else
+            {
+                toggled = outputGameObject.activeSelf;
+            }
+        }
+
+        public override void OnPlayerRestored(VRCPlayerApi player)
+        {
+            if (_isPersisted && _persistenceIdentifier != null)
+            {
+                if (player == Networking.LocalPlayer)
+                {
+                    LoadPersistentState();
+                }
+            }
+        }
+
+        public override void OnDeserialization()
+        {
+            if (_isSynced)
+            {
+                if (!Networking.IsOwner(gameObject))
+                {
+                    outputGameObject.SetActive(toggledSynced);
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (_onEnableActive) { OnTrigger(); }
+        }
+
+        private void OnDisable()
+        {
+            if (_onDisableActive) { OnTrigger(); }
+        }
+
+        public void OnTrigger()
+        {
+            Debug.Log("BW - Event Triggered");
+            if (outputMessageBehaviour != null)
+            {
+                CastEvent();
+            }
+
+            if (outputGameObject != null)
+            {
+                CastToggle();
+            }
+        }
+
+        private void CastEvent()
+        {
+            outputMessageBehaviour.SendCustomEvent(_outputMessage);
+            Debug.Log("BW - Message <" + _outputMessage + "> casted to <" + outputMessageBehaviour + ">");
+        }
+
+        private void CastToggle()
+        {
+            Debug.Log("BW - GameObject <" + outputGameObject + "> toggled. Synced " + _isSynced + ". Persisted " + _isPersisted + ".");
+
+            if (_isSynced)
+            {
+                if (!Networking.IsOwner(gameObject)) Networking.SetOwner(Networking.LocalPlayer, gameObject);
+                toggledSynced = !toggledSynced;
+                RequestSerialization();
+                outputGameObject.SetActive(toggledSynced);
+            }
+            else
+            {
+                toggled = !toggled;
+                outputGameObject.SetActive(toggled);
+
+                if (_isPersisted)
+                {
+                    SavePersistentState();
+                }
+            }
+        }
+
+        private void LoadPersistentState()
+        {
+            VRCPlayerApi localPlayer = Networking.LocalPlayer; // Get the local player.
+            if (localPlayer != null && PlayerData.TryGetBool(localPlayer, _persistenceIdentifier, out bool persistedState))
+            {
+                toggled = persistedState;
+                outputGameObject.SetActive(toggled);
+            }
+            else
+            {
+                // If no persistent data exists, initialize with the GameObject's current state.
+                toggled = outputGameObject.activeSelf;
+                SavePersistentState(); // Save the initial state.
+            }
+        }
+
+        private void SavePersistentState()
+        {
+            VRCPlayerApi localPlayer = Networking.LocalPlayer; // Get the local player.
+            if (localPlayer != null)
+            {
+                PlayerData.SetBool(_persistenceIdentifier, toggled);
+            }
+        }
+    }
+}
